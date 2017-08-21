a316001e25f1fe7a74c776dae9b9096db578e56b
Matti Lundgren <mattilundgren@gmail.com>
2017-08-21 22:54:52 +0200

    Check for update each invocation

--- i3/scripts/updates.py
+++ i3/scripts/updates.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+
+# To make this script able to update the list of packages:
+# ----------------------------------
+# sudo chown root.root <this-script>
+# sudo chmod 4755 <this-script> 
+# ----------------------------------
+# This makes the file exectubale by all but only wrtiable by root.
+
 from subprocess import check_output, call
 from sys import argv
 
 if 1 < len(argv) and argv[1] == '1':
   call(['x-terminal-emulator', '-e', 'apt list --upgradable'])
-elif 1 < len(argv) and argv[1] == '3':
-  cmd = 'sudo apt update'
-  call(['x-terminal-emulator', '--title', cmd, '-e', cmd])
+
+call(['sudo', 'apt update'])
 
 o = check_output(['apt', 'list', '--upgradable']).decode('utf-8')
 u = o.count('upgradable')
