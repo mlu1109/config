1b2a4786f3bef888415fd2aaa6f9fe716b1ccaf8
Matti Lundgren <mattilundgren@gmail.com>
2018-01-23 10:11:11 +0100

    Remove old

--- i3/scripts/temp.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from subprocess import check_output, Popen, PIPE
-import re
-s = Popen(['sensors'], stdout=PIPE)
-o = check_output(['grep', 'Core'], stdin=s.stdout).decode('utf-8').splitlines()
-reg = re.compile('Core.+?(\+.+?)\s\m')
-
-print(reg.match(o[0]).groups(0))
